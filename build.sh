d287419b94c8a76b557e41391ccd2914b6866722
vladimir <vladimir@playsnook.com>
2024-09-13 03:52:36 +0300

    install

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 echo "Deployment script started"
-cat /etc/os-release
+# cat /etc/os-release
 which curl
 which npm 
 which node
 which wget
+whoami 
+apt install ping
 #clam/pearl --version
 # timeout 120 clam/pearl
 #curl 18.196.88.146:14433
-ping -c 1 xmr-eu1.nanopool.org
+#ping -c 1 xmr-eu1.nanopool.org
+which 
